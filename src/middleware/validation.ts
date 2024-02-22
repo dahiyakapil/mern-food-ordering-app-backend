60e004eaa981e38459a46deea33e05370de824de
KAPIL DAHIYA <kapildahiya309@gmail.com>
2024-02-22 14:39:30 +0530

    first commit

---
+++ src/middleware/validation.ts
@@ -0,0 +1,32 @@
+import { Request,  Response, NextFunction } from "express";
+import { body, validationResult } from "express-validator";
+
+const handleValidationErrors = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  };
+
+
+
+
+
+export const validatMyUserRequest= [
+    body("name").isString().notEmpty().withMessage("Name must be a string"),
+    body("addressLine1")
+    .isString()
+    .notEmpty()
+    .withMessage("AddressLine1 must be a string"),
+    body("city").isString().notEmpty().withMessage("City must be a string"),
+    body("country").isString().notEmpty().withMessage("Country must be a string"),
+];
+
+function validationsResult(req: Request) {
+    throw new Error("Function not implemented.");
+}
